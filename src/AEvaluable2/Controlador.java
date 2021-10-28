bd98abfcf52aa65b1058ad52f065d9de10a58622
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-10-28 21:15:59 +0200

    fichero modelo implementado

--- src/AEvaluable2/Controlador.java
+++ src/AEvaluable2/Controlador.java
@@ -1,8 +1,22 @@
 package AEvaluable2;
 
+import javax.swing.event.AncestorListener;
+
 public class Controlador {
 
+	private Modelo modelo;
+	private Vista vista;
+	private AncestorListener actionListenesBuscar, actionListenerReemplazar;
+	private String ficheroLectura, ficheroEscritura;
+	private String textoBuscar, textoReemplazar;
+
 	public Controlador(Modelo modelo, Vista vista) {
+		this.modelo = modelo;
+		this.vista = vista;
+		control();
+	}
+	
+	public void control() {
 		
 	}
 
--- src/AEvaluable2/Modelo.java
+++ src/AEvaluable2/Modelo.java
@@ -1,8 +1,10 @@
 package AEvaluable2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -14,19 +16,71 @@ public class Modelo {
 	private String fichero_escritura;
 
 	public Modelo() {
-		fichero_lectura="AE02_T1_2_Streams_Groucho.txt";
-		fichero_escritura= "ficheroEscritura.txt";
+		fichero_lectura = "AE02_T1_2_Streams_Groucho.txt";
+		fichero_escritura = "ficheroEscritura.txt";
 	}
-	
+
 	public ArrayList<String> contenidoFichero(String fichero) {
 		ArrayList<String> contenidoFichero = new ArrayList<String>();
 		File f = new File(fichero);
 		try {
-			FileReader fr =new FileReader(f);
+			FileReader fr = new FileReader(f);
 			BufferedReader br = new BufferedReader(fr);
 			String linea = br.readLine();
-			while(linea != null) {
+			while (linea != null) {
 				contenidoFichero.add(linea);
+				linea = br.readLine();
+			}
+			br.close();
+			fr.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		return contenidoFichero;
+	}
+
+	public String ficheroLectura() {
+		return fichero_lectura;
+	}
+
+	public String ficheroEscritura() {
+		return fichero_escritura;
+	}
+
+	public int buscarTexto(String textoBuscar) {
+		int contador = 0;
+		File f = new File(ficheroLectura());
+		try {
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			String linea = br.readLine();
+			while (linea != null) {
+				while (linea.indexOf(textoBuscar) > -1) {
+					linea = linea.substring(linea.indexOf(textoBuscar) + textoBuscar.length(), linea.length());
+					contador++;
+				}
+				linea = br.readLine();
+			}
+			br.close();
+			fr.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		return contador;
+	}
+
+	public void reemplazarTexto(String textoBuscar, String textoReemplazar) {
+		File f1 = new File(ficheroLectura());
+		File f2 = new File(ficheroEscritura());
+		try {
+			FileReader fr = new FileReader(f1);
+			BufferedReader br = new BufferedReader(fr);
+			FileWriter fw = new FileWriter(f2);
+			BufferedWriter bw = new BufferedWriter(fw);
+			String linea = br.readLine();
+			while(linea != null) {
+				bw.write(linea.replaceAll(textoBuscar, textoReemplazar));
+				bw.newLine();
 				linea = br.readLine();
 			}
 			br.close();
@@ -34,6 +88,5 @@ public class Modelo {
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);	
 		}
-		return contenidoFichero;
 	}
 }
